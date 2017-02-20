945e230466f58e4ce64969e89f15b4b08b0d26ae
xianling zhang <alchemxz@gmail.com>
2017-02-19 23:37:06 -0500

    Create XMLData.cs

---
+++ XMLData.cs
@@ -0,0 +1,11 @@
+
+public class XMLData{
+	public int pageNum;
+	public string CharText, dialogueText;
+	public XMLData(int page, string character, string dialogue)
+	{
+	pageNum=page;
+	charText=character;
+	dialogueText=dialogue;
+	}
+}
